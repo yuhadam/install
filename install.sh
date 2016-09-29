c25e9ef74acf3c04bf774f142e48812639153e0b
IchthysNgs <mjkam@naver.com>
2016-09-29 15:41:56 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 yum -y update
 
-wget -no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u60-b27/jdk-8u60-linux-x64.rpm"
+wget --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u102-b14/jdk-8u102-linux-x64.rpm
 
-yum localinstall jre-8u60-linux-x64.rpm
+yum localinstall jdk-8u102-linux-x64.rpm
 
 
 git clone https://www.github.com/ichthysngs/install /install
