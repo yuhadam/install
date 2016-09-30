6aab80feb22badc4028f7cc2f8e9dd034eb6872d
IchthysNgs <mjkam@naver.com>
2016-09-30 11:12:42 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,11 +4,7 @@ yum -y update
 
 wget --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u102-b14/jdk-8u102-linux-x64.rpm
 
-yum localinstall jdk-8u102-linux-x64.rpm
-
-wget http://downloads.typesafe.com/scala/2.11.6/scala-2.11.6.rpm
-
-rpm -ivh /data/scala-2.11.6.rpm
+yum localinstall -y jdk-8u102-linux-x64.rpm
 
 git clone https://www.github.com/ichthysngs/install /install
 
