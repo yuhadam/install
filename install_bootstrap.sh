71a3600ea5638ef2d3286bb4dd4c7e70e4f4988c
IchthysNgs <mjkam@naver.com>
2016-10-26 14:42:39 +0900

    Update install_bootstrap.sh

--- install_bootstrap.sh
+++ install_bootstrap.sh
@@ -34,7 +34,7 @@ done
 
 for((i=3;i<$arrlen;i++))
 do
-ssh root@${array[$i]} "yum -y update && yum install -y git && cd /root/ && git clone http://www.github.com/ichthysngs/install2 && cd install2 && ./install_nobootstrap.sh"
+ssh root@${array[$i]} "yum -y update && yum install -y git && cd /root/ && git clone http://www.github.com/ichthysngs/install2 && cd install2 && ./install_nobootstrap.sh $masterip"
 echo "#####finish#####"
 done
 
